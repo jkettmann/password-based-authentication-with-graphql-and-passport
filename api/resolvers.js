a35b2e34cd592cfefc2f48a2b518b002ca0e3958
Johannes Kettmann <jkettmann@gmx.net>
2020-01-27 08:03:53 +0100

    Fix asynchonous login call

--- api/resolvers.js
+++ api/resolvers.js
@@ -5,7 +5,7 @@ const resolvers = {
     currentUser: (parent, args, context) => context.getUser(),
   },
   Mutation: {
-    signup: (parent, { firstName, lastName, email, password }, context) => {
+    signup: async (parent, { firstName, lastName, email, password }, context) => {
       const existingUsers = context.User.getUsers();
       const userWithEmailAlreadyExists = !!existingUsers.find(user => user.email === email);
 
@@ -23,13 +23,13 @@ const resolvers = {
 
       context.User.addUser(newUser);
 
-      context.login(newUser);
+      await context.login(newUser);
 
       return { user: newUser };
     },
     login: async (parent, { email, password }, context) => {
       const { user } = await context.authenticate('graphql-local', { email, password });
-      context.login(user);
+      await context.login(user);
       return { user }
     },
     logout: (parent, args, context) => context.logout(),
